f2d34a8af2e1b24efd8f83fabd9a6bdb7a8ad607
vuvumiu <vu122004@gmail.com>
2025-05-15 13:25:41 +0700

    DemoHelloWorld

---
+++ BTH1/src/main/java/org/example/demohelloworld/DemoHelloworldApplication.java
@@ -0,0 +1,13 @@
+package org.example.demohelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloworldApplication.class, args);
+    }
+
+}
---
+++ BTH1/src/test/java/org/example/demohelloworld/DemoHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
